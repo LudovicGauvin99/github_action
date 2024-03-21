b896a306f35d84769f2e37bce7a2fdc55026db5d
LudovicGauvin99 <lulu91340@hotmail.fr>
2024-03-21 11:53:07 +0100

    fourth

--- job.py
+++ job.py
@@ -1,2 +1,11 @@
-a = 2
-print("coucou", a)
+values = (5, 6, 8, 1, 50, 32, 42, 51, 6, 8)
+
+def mean(table):
+    if  (len(table) == 0):
+        return 0
+
+    total = 0
+    for n in table:
+        total += n
+
+    return total / len(table)
