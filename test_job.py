feeabc0f134b23cced260c888893384bbf104057
LudovicGauvin99 <lulu91340@hotmail.fr>
2024-03-21 11:57:33 +0100

    fourth

--- test_job.py
+++ test_job.py
@@ -1,7 +1,7 @@
 import pytest
 from job import job
 
-def test_mean():
+def test_job():
     test_cases = [
         ([1, 2, 3, 4, 5], 3.0),
         ([5, 5, 5, 5, 5], 5.0),
@@ -11,5 +11,5 @@ def test_mean():
         ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5.5),
     ]
 
-    for nums, expected_mean in test_cases:
-        assert mean(nums) == expected_mean
+    for nums, expected_job in test_cases:
+        assert job(nums) == expected_job
